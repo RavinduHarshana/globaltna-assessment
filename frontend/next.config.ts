a55ac28cdd7f7f03e28ee39820dbb2f159e3c55d
Ravindu Harshana <ravinduharshana2001@gmail.com>
2026-05-16 13:32:49 +0530

    feat: initialize frontend with Next.js, TypeScript, and Tailwind CSS

---
+++ frontend/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: ['169.254.204.13'],
+
+};
+
+
+export default nextConfig;
