c68d47325f39ef8447f49d722695db241560c813
Ravindu Harshana <ravinduharshana2001@gmail.com>
2026-05-17 08:37:59 +0530

    feat: implement authentication system with user registration and login

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import User from '../models/User';
+
+
+const generateToken = (id: string) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET as string, {
+        expiresIn: '30d',
+    });
+};
+
+//Register a new user
+export const registerUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const { name, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({ name, email, password: hashedPassword });
+
+        return res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user.id),
+        });
+    } catch (error) {
+        return res.status(500).json({ message: 'Error registering user', error });
+    }
+};
+
+//login user
+export const loginUser = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const { email, password } = req.body;
+
+
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password as string))) {
+            return res.status(200).json({
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user.id),
+            });
+        } else {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Error logging in', error });
+    }
+};
