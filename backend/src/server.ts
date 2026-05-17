50b1bbf068e0811e8b8e0664164adabccaf7ede4
Ravindu Harshana <ravinduharshana2001@gmail.com>
2026-05-17 08:37:24 +0530

    feat: implement server setup with Express, MongoDB connection, and routing

---
+++ backend/src/server.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import jobRoutes from './routes/jobRoutes';
+import authRoutes from './routes/authRoutes';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/jobs', jobRoutes);
+
+const mongoURI = process.env.MONGO_URI as string;
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas successfully');
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
